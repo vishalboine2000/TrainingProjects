c7a6edad60afaf2bacb1846ce7a284a6677aed08
vishalboine2000 <77009906+vishalboine2000@users.noreply.github.com>
2021-01-15 18:15:41 +0700

    Add files via upload

---
+++ Bank Application/src/com/capgemini/bean/Account.java
@@ -0,0 +1,26 @@
+package com.capgemini.bean;
+
+public class Account 
+{
+	private int accountNumber;
+	private int amount;
+	public Account(int accountNumber, int amount) {
+		super();
+		this.accountNumber = accountNumber;
+		this.amount = amount;
+	}
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+	
+}
---
+++ Bank Application/src/com/capgemini/service/Bank.java
@@ -0,0 +1,14 @@
+package com.capgemini.service;
+import com.capgemini.exception.InsufficientBalanceException;
+import com.capgemini.exception.InsufficientOpeningAmountException;
+import com.capgemini.exception.InvalidAccountNumberException;
+
+
+public interface Bank {
+	public String createAccount(int accountNumber,int amount) throws InsufficientOpeningAmountException;
+	public int withdrawAmount(int accountNumber,int amount) throws InvalidAccountNumberException, InsufficientBalanceException;
+	public int[] fundTransfer(int senderAccountNumber,int recieverAcoountNumber,int transferAmount) throws InvalidAccountNumberException, InsufficientBalanceException;
+	public int depositAmount(int accountNumber,int amount)throws InvalidAccountNumberException;
+
+}
+
